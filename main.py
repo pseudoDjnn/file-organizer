c3f73bf6a2c4b2616eb0f0ffb2c3ce774f2b031a
pseudoDjnn <lavahrexes@gmail.com>
2025-04-25 13:19:33 -0400

    update folders and the names for them

--- main.py
+++ main.py
@@ -3,10 +3,19 @@ import shutil
 from pathlib import Path
 
 def main():
+    # Get the user's home directory
     home = Path.home()
 
+    # Define the Desktop folder path
     desktop = home / "Desktop"
     print("Desktop Path:", desktop)
+    folder = {
+        "Downloads": home / "Downloads",
+        "Documents": home / "Documents",
+        "Pictures": home / "Pictures",
+        "Music": home / "Music",
+        "Downloads": home / "Downloads",
+    }
     
 if __name__=="__main__":
     main()
