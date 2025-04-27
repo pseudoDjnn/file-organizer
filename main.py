ee858add1f0b3833172066bb51762e4d08eaa054
pseudoDjnn <lavahrexes@gmail.com>
2025-04-26 23:25:22 -0400

    update get_file_mapping

--- main.py
+++ main.py
@@ -19,7 +19,25 @@ def get_system_folders():
     
     
 def get_file_mapping(system_folders: dict):
-    pass
+    return {
+        ".txt": system_folders["Documents"],
+        ".pdf": system_folders["Documents"],
+        ".doc": system_folders["Documents"],
+        ".docx": system_folders["Documents"],
+        
+        ".jpg": system_folders["Pictures"],
+        ".jpeg": system_folders["Pictures"],
+        ".png": system_folders["Pictures"],
+        ".gif": system_folders["Pictures"],
+        
+        ".mp3": system_folders["Music"],
+        ".wav": system_folders["Music"],
+        ".flac": system_folders["Music"],
+        
+        ".mp4": system_folders["Videos"],
+        ".mov": system_folders["Videos"],
+        ".avi": system_folders["Videos"],
+    }
     
     
 def main():
