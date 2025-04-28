f341f90b0a5763387a3ec200e6c70b96afb6f048
pseudoDjnn <lavahrexes@gmail.com>
2025-04-28 12:05:50 -0400

    update for OneDrive and lack thereof

--- main.py
+++ main.py
@@ -4,10 +4,18 @@ from pathlib import Path
 
 def get_desktop_directory():
     # Get the user's home directory
-    windows_desktop = Path("/mnt/c/Users/lavah/OneDrive/Desktop")
-    if windows_desktop.exists():
-        print("Using Windows Desktop Path:", windows_desktop)
-        return windows_desktop
+    one_drive_desktop = Path("/mnt/c/Users/lavah/OneDrive/Desktop")
+    standard_windows_desktop = Path("/mnt/c/Users/lavah/Desktop")
+    
+    if one_drive_desktop.exists():
+        print("Using Windows Desktop Path:", one_drive_desktop)
+        return one_drive_desktop
+    else:
+        print("Warning: Windows OneDrive not found.")
+        
+    if standard_windows_desktop.exists():
+        print("Using Windows Desktop Path:", standard_windows_desktop)
+        return standard_windows_desktop
     else:
         print("Warning: Windows Desktop path not found.")
     
