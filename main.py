f72c46a061db7fd61d8d7d6f26f9d99025091567
pseudoDjnn <lavahrexes@gmail.com>
2025-04-30 23:49:09 -0400

    seeing if this will work shortly with my setup

--- main.py
+++ main.py
@@ -12,7 +12,31 @@ FILE_CATEGORIES = {
 }
     
 def organize_files(directory):
-    pass
+    """
+    Organizes file sin the given dir based on their file type
+    """
+    if not os.path.isdir(directory):
+        raise Exception(f"Error: {directory} is not a valid directory")
+
+    for category in FILE_CATEGORIES:
+        folder_path = os.path.join(directory, category)
+        os.makedirs(folder_path, exist_ok=True)
+        
+    for filename in os.listdir(directory):
+        file_path = os.path.join(directory, filename)
+        
+        if os.path.isdir(file_path):
+            continue
+
+        file_moved = False
+        for category, extentions in FILE_CATEGORIES.items():
+            if any(filename.lower().endswith(ext) for ext in extentions):
+                shutil.move(file_path, os.path.join(directory, category, filename))
+                file_moved = True
+                break
+            
+        if not file_moved:
+            shutil.move(file_path, os.path.join(directory, "Others", filename))
 
 
 def main():
