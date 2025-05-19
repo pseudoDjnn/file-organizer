57a97161827b0702c082e38cc4b7adb7f123da57
pseudoDjnn <lavahrexes@gmail.com>
2025-05-19 12:33:58 -0400

    updated main.py

--- main.py
+++ main.py
@@ -1,12 +1,27 @@
+import logging
 from file_organizer import organize_files
+
+logging.basicConfig(
+    filename="app.log",
+    
+    level=logging.INFO,
+    format=""
+)
 
 def main():
     """
     Main execution function prompting user for a directory path.
     """
-    directory_to_organize = input("Enter the directory path to organize: ")
-    organize_files(directory_to_organize)
 
+    logging.info("File Organizer started.")
+    
+    directory_to_organize = input("Enter the directory path to organize: ")
+    try:
+        organize_files(directory_to_organize)
+        logging.info(f"Files organized in directory: {directory_to_organize}")
+    except Exception as e:
+        logging.error(f"An error occurred during the organization: {e}")
+        print(f"An error occured: {e}")
     
 if __name__ == "__main__": 
     main()
