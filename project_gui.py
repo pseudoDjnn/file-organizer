bfb1c40e58918b3ff0a521624e38675d463862a6
pseudoDjnn <lavahrexes@gmail.com>
2025-05-20 11:09:36 -0400

    set up central layout

--- project_gui.py
+++ project_gui.py
@@ -20,4 +20,35 @@ class GuiForProject(QMainWindow):
         
     
     def initUI(self):
+        main_widget = QWidget()
+        layout = QVBoxLayout()
+
+        # Status for updates
+
+        self.status_label = QLabel("Select a directory to organize.")
+        layout.addWidget(self.status_label)
+        
+        # Browse button for selecting a directory
+        
+        browse_button = QPushButton("Browse Directory")
+        browse_button.clicked.connect(self.browse_directory)
+        layout.addWidget(browse_button)
+        
+        # Run organizer button
+        
+        run_button = QPushButton("Run Organizer")
+        run_button.clicked.connect(self.run_organizer)
+        layout.addWidget(run_button)
+
+        main_widget.setLayout(layout)
+        self.setCentralWidget(main_widget)
+        self.apply_styles()
+        
+    def browse_directory(self):
+        pass
+    
+    def run_organizer(self):
+        pass
+
+    def apply_styles(self):
         pass
