61969c4fda615cd3ecc68f3e165560a02ec5d6de
pseudoDjnn <lavahrexes@gmail.com>
2025-11-21 18:14:14 -0500

    setting up models.py

---
+++ services/models.py
@@ -0,0 +1,12 @@
+import os
+import datetime
+
+class FileItem:
+    def __init__(self, path):
+        pass
+        
+    def year_created(self):
+        pass
+    
+    def __repr__(self):
+        pass
