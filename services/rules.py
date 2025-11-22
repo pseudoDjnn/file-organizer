243c233873e37ee14b061fcfb75dacb5cd5b31a5
pseudoDjnn <lavahrexes@gmail.com>
2025-11-22 17:37:09 -0500

    filter_excluded grabs our FileItem and returns it only with our configured parameters

--- services/rules.py
+++ services/rules.py
@@ -1,7 +1,11 @@
 import os
 
-def filter_exclude(files, excluded_items, excluded_ext):
-    pass
+def filter_excluded(files, excluded_items, excluded_ext):
+    return [
+        file for file in files
+        if file.name not in excluded_items
+        and not file.name.lower().endswith(tuple(excluded_ext))
+    ]
 
 def group_by_date(files, depth="month"):
     pass
