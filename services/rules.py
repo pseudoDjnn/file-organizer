5bae5b1a05775f22f6c96e743fd057270bfb573e
pseudoDjnn <lavahrexes@gmail.com>
2025-11-22 18:54:21 -0500

    group by date function will take the files we want and now group them respective to the constraints

--- services/rules.py
+++ services/rules.py
@@ -1,4 +1,6 @@
 import os
+from services.models import FileItem
+
 
 def filter_excluded(files, excluded_items, excluded_ext):
     return [
@@ -8,7 +10,22 @@ def filter_excluded(files, excluded_items, excluded_ext):
     ]
 
 def group_by_date(files, depth="month"):
-    pass
+#  Empty dictyionary we append into later in the func
+    grouped = {}
+#  Move through out files with a loop
+    for file in files:
+    # This is looking only for the "year"
+        if depth == "year":
+            key = file.year_created()
+    # This is looking for the "year" and "month"
+        elif depth == "month":
+            key = (file.year_created(), file.month_created())
+    # This is looking for the "year" and "month" and "day"
+        elif depth == "day":
+            key = (file.year_created(), file.month_created(), file.day_created())
+    #  Insert the logic into the dictionary insuring there is always a list at the key
+        grouped.setdefault(key, []).append(file)    
+    return grouped
 
 def build_destination(base_dir, file, depth="month"):
     pass
