11cca3cc0c7ff2a633355e433212bb573e584b12
pseudoDjnn <lavahrexes@gmail.com>
2025-11-22 16:39:30 -0500

    setting up rules.py

---
+++ services/rules.py
@@ -0,0 +1,10 @@
+import os
+
+def filter_exclude(files, excluded_items, excluded_ext):
+    pass
+
+def group_by_date(files, depth="month"):
+    pass
+
+def build_destination(base_dir, file, depth="month"):
+    pass
